544a094b05e20a37bf283eee90584e589cbaea11
Thegreen0ne <max.evangeliste@gmail.com>
2015-01-19 19:37:45 -0500

    Modif in User and Article models

--- app/models/User.php
+++ app/models/User.php
@@ -6,21 +6,9 @@ use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
 
 class User extends Eloquent implements UserInterface, RemindableInterface {
-
 	use UserTrait, RemindableTrait;
-
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
+	
+	protected $fillable = ['username', 'password', 'email'];
 	protected $table = 'users';
-
-	/**
-	 * The attributes excluded from the model's JSON form.
-	 *
-	 * @var array
-	 */
-	protected $hidden = array('password', 'remember_token');
-
+	protected $hidden = array('password');
 }
