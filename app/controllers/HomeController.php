c13ed0459ccfc3ff5d3d508dd17beb0178382e54
Thegreen0ne <max.evangeliste@gmail.com>
2015-02-03 20:47:05 -0500

    \User test

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -8,7 +8,7 @@ class HomeController extends BaseController {
 	 *
 	 * @return Response
 	 */
-	public function getIndex($id = null) 
+	public function getIndex(asdasda $id = null) 
 	{
 		$articles = $this->initializeSidebarArticles();
 		return View::make('pages.index')
