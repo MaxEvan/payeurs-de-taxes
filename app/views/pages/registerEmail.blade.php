c580e0fbbea4d00e8163e895dcab7830eabc755c
Thegreen0ne <max.evangeliste@gmail.com>
2015-03-27 14:26:29 -0400

    forgot registerEmail.blade.php in last commit

---
+++ app/views/pages/registerEmail.blade.php
@@ -0,0 +1,11 @@
+@extends('templates.formPage')
+
+@section('content')
+<div class="text-center">
+        <h1>Votre compte a été créé.</h1>
+        <br>
+        <p>Un message a été envoyé à votre adresse courriel. <br> Cliquez sur le lien dans le message pour activer votre compte.</p>
+        <br>
+        <a href="/">Retourner à la page d'accueil</a>
+</div>
+@stop
